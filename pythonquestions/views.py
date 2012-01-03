3b12c666692d98444f37d377a53e4e7cd499e359
Dan Crosta <dcrosta@late.am>
2012-01-02 20:28:36 -0500

    track click counts

--- pythonquestions/views.py
+++ pythonquestions/views.py
@@ -9,6 +9,10 @@ def sayit():
 
 @app.route('/so/<int:question_id>')
 def redirect_stackoverflow(question_id):
-    mongo.db.questions.stackoverflow.find_one_or_404(question_id)
+    question = mongo.db.questions.stackoverflow.find_one_or_404(question_id)
+    mongo.db.questions.stackoverflow.update(
+        {'_id': question['_id']},
+        {'$inc': {'clicks': 1}},
+        safe=False)
     return redirect('http://stackoverflow.com/questions/%s' % question_id, 301)
 
